c5bb9c17780c13565d5768865eeb4aabd75100bd
Bruno Caitano <snowrunescape@snowdev.com.br>
2023-10-01 14:17:13 -0400

    feat: implements more tests of cart

--- tests/cart.test.ts
+++ tests/cart.test.ts
@@ -4,7 +4,6 @@ describe("Cart", () => {
     let sampleProductId: number;
 
     beforeAll(() => {
-        // Assuming you have a product ID to use for testing
         sampleProductId = 1;
     });
 
@@ -17,42 +16,56 @@ describe("Cart", () => {
     });
 
     test("add product to cart", () => {
-        // Arrange
-        const initialCart = cart.getCart();
+        cart.addProduct(sampleProductId);
 
-        // Act
-        cart.addProduct(sampleProductId, 1);
-
-        // Assert
         const updatedCart = cart.getCart();
-        expect(updatedCart.items.length).toBe(initialCart.items.length + 1);
+
+        expect(updatedCart.items.length).toBe(1);
+        expect(updatedCart.items.some(item => item.id === sampleProductId)).toBe(true);
+    });
+
+    test("add and re-add product to cart", () => {
+        cart.addProduct(sampleProductId);
+        cart.addProduct(sampleProductId);
+
+        const updatedCart = cart.getCart();
+
+        expect(updatedCart.items.length).toBe(1);
         expect(updatedCart.items.some(item => item.id === sampleProductId)).toBe(true);
     });
 
     test("update amount in cart", () => {
-        // Arrange
         const initialCart = cart.getCart();
         const originalAmount = initialCart.items.find(item => item.id === sampleProductId)?.amount || 1;
 
-        // Act
         cart.updateAmountProduct(sampleProductId, originalAmount + 1);
 
-        // Assert
         const updatedCart = cart.getCart();
         const updatedAmount = updatedCart.items.find(item => item.id === sampleProductId)?.amount;
         expect(updatedAmount).toBe(originalAmount + 1);
     });
 
-    test("remove product from cart", () => {
-        // Arrange
+    test("update amount of product don't exists on cart", () => {
         const initialCart = cart.getCart();
 
-        // Act
+        cart.updateAmountProduct(999, 3);
+
+        expect(cart.getCart()).toEqual(initialCart);
+    });
+
+    test("remove product from cart", () => {
+        const initialCart = cart.getCart();
+
         cart.removeProduct(sampleProductId);
 
-        // Assert
         const updatedCart = cart.getCart();
         expect(updatedCart.items.length).toBe(initialCart.items.length - 1);
         expect(updatedCart.items.some(item => item.id === sampleProductId)).toBe(false);
+    });
+
+    test.skip("finish order", () => {
+        // Placeholder test for the 'finish order' functionality.
+        // This test will be implemented in the future to ensure that the order finishing logic works correctly.
+        // You can provide additional comments or details about the expected behavior.
     });
 });
