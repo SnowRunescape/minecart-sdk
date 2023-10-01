566ea02fca4aeb28f3bc10d0afcc735e5b30380f
Bruno Caitano <snowrunescape@snowdev.com.br>
2023-10-01 11:30:44 -0400

    fix: types

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,28 @@
-import { gateways } from "./src/gateways";
-import { products } from "./src/products";
-import { cart } from "./src/cart";
-
 declare module "minecart-sdk" {
+    export type Cart = {
+        username: string | null,
+        items: Item[],
+    }
+
+    export type Item = {
+        id: number,
+        amount: number,
+    }
+
     export const minecart: {
-        products: typeof products;
-        gateways: typeof gateways;
-        cart: typeof cart;
+        products: {
+            all(filters?: Record<string, string | number>): Promise<any[]>;
+        };
+        gateways: {
+            all(): Promise<any>;
+        };
+        cart: {
+            getCart(): Cart;
+            setUsername(username: string): void;
+            addProduct(productId: number, amount?: number): void;
+            updateAmountProduct(productId: number, amount: number): void;
+            removeProduct(productId: number): void;
+            finishOrder(gateway: string): string;
+        };
     };
 }
